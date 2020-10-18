5e706fca1f480ae15bba8c82286ddd3a8c9aab37
Nesterov Oleg <epujsv1973@gmail.com>
2020-10-18 19:10:21 +0300

    Test waitable pool with task and thread

--- Test/WaitablePoolTest/Program.cs
+++ Test/WaitablePoolTest/Program.cs
@@ -11,13 +11,19 @@ namespace WaitablePoolTest
     {
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(100, 100);
+            CheckUsingTask();
+        }
+
+        private static void CheckUsingThread()
+        {
             var syncPool = new WaitablePool(5, 1);
 
             var callStats = new ConcurrentDictionary<int, DateTimeOffset>();
 
             for (int i = 0; i < 100; i++)
             {
-                
+
                 new Thread(id =>
                 {
                     var highPriority = id != null && (int)id % 10 == 0;
@@ -37,6 +43,42 @@ namespace WaitablePoolTest
                             : $"{name} finished. The locker {locker.Id} was used first time");
                     callStats[locker.Id] = dt;
                 }).Start(i);
+
+                Thread.Sleep(100);
+            }
+
+            Console.ReadKey();
+            syncPool.Dispose();
+        }
+
+        private static void CheckUsingTask()
+        {
+            var syncPool = new WaitablePool(5, 1);
+
+            var callStats = new ConcurrentDictionary<int, DateTimeOffset>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var id = i;
+                Task.Run(() =>
+                {
+                    var highPriority = id % 10 == 0;
+                    var name = highPriority
+                        ? $"High Priority thread #{id}"
+                        : $"Standard thread #{id}";
+                    Console.WriteLine($"{name} was started");
+
+                    // ReSharper disable once AccessToDisposedClosure
+                    var locker = syncPool.Wait(highPriority);
+                    var dt = DateTimeOffset.UtcNow;
+                    locker.UnlockAfterMs(5000);
+
+                    Console.WriteLine(
+                        callStats.TryGetValue(locker.Id, out var lastCall)
+                            ? $"{name} finished. The locker {locker.Id} was used after a timeout of {(dt - lastCall).TotalMilliseconds:F0} ms"
+                            : $"{name} finished. The locker {locker.Id} was used first time");
+                    callStats[locker.Id] = dt;
+                });
 
                 Thread.Sleep(100);
             }
